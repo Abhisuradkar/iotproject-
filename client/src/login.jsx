8e60438f84c8738b97eb82eacd8d647dd926e1dd
Abhishek Suradkar <suradkarabhishek57@gmail.com>
2026-03-29 12:50:29 +0530

    asf

--- client/src/login.jsx
+++ client/src/login.jsx
@@ -4,7 +4,7 @@ import axios from "axios";
 export default function Login({ setToken }) {
 
   // ✅ ADD THIS (your live backend)
-// const API = import.meta.env.VITE_API_URL;
+ const API = import.meta.env.VITE_API_URL;
 
   const [form, setForm] = useState({
     name: "",
